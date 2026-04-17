b8b7319d43d1e1d7a2c71388bd7434c5174492e4
waltercarroll06 <wscn2006@gmail.com>
2026-04-16 21:31:34 -0500

    1

---
+++ trabajogithub.py
@@ -0,0 +1,32 @@
+# nombre usuario
+
+nombre_usuario = input("ingresa tu nombre: ")
+# meta usuario
+
+meta_usuario = input("ingresa tu meta: ")
+
+#mensaje de bienvenida
+
+print(f"¡Bienvenid@ {nombre_usuario} a Control de Hábitos! Un paso pequeño cada día puede cambiar tu vida.")
+
+# funciones 
+
+def mostrar_menu():
+    pass
+
+def ver_habitos(habitos):
+    pass
+
+def agregar_habito(habitos):
+        nombre_habito = input("ingresa tu nombre: ").strip()
+        
+    
+
+def completar_habito(habitos):
+    pass
+
+def eliminar_habito(habitos):
+    pass
+
+def ver_progreso(habitos):
+    pass
